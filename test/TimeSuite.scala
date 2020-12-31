2f3a258094d7d6bf1db18f2efee31d25c1db36c8
stephentetley <stephen.tetley@gmail.com>
2020-12-31 11:33:34 +0000

    Updating tests.

--- test/TimeSuite.scala
+++ test/TimeSuite.scala
@@ -20,7 +20,8 @@ import ca.uwaterloo.flix.util.{FlixTest, Options}
 import org.scalatest.Suites
 
 class TimeSuite extends Suites(
-  new FlixTest("TestIsoUnit", "main/test/spt/newtime/TestIsoUnit.flix")(Options.TestWithLibrary),
+  new FlixTest("TestTemporalIsoUnit", "main/test/spt/newtime/TestTemporalIsoUnit.flix")(Options.TestWithLibrary),
   new FlixTest("TestDayOfWeek", "main/test/spt/newtime/TestDayOfWeek.flix")(Options.TestWithLibrary),
   new FlixTest("TestDuration", "main/test/spt/newtime/TestDuration.flix")(Options.TestWithLibrary),
+  new FlixTest("TestInstant", "main/test/spt/newtime/TestInstant.flix")(Options.TestWithLibrary),
 )
